d69df5c9fa0c70dcb9afb689d5c7c04b11b3d66b
EC2 Default User <ec2-user@ip-172-31-46-87.ap-northeast-1.compute.internal>
2022-05-05 13:55:16 +0000

    book detail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,26 @@
 class UsersController < ApplicationController
+  
   def index
   end
 
   def show
     @user = User.find(params[:id])
+    @book = Book.new
   end
 
   def edit
     @user = User.find(params[:id])
   end
   
+ 
+  
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path
+    redirect_to user_path(@user.id)
   end
 
-private
+  private
   # ストロングパラメータ
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
