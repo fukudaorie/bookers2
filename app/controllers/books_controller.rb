56aca2c6fd5ab127c623eb2062596fd76646fad5
EC2 Default User <ec2-user@ip-172-31-46-87.ap-northeast-1.compute.internal>
2022-05-09 14:37:44 +0000

    login Flashメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,18 +4,28 @@ class BooksController < ApplicationController
   end
   
   def create
-   book = Book.new(book_params)
-   book.user_id = current_user.id
-   book.save
-   redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
   
   def index
+    @book = Book.new
     @books = Book.all
+    @user = current_user
   end
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
+    @user = @book.user
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,14 @@ class UsersController < ApplicationController
   
   def index
     @users = User.all
+    @user = current_user
+    @book_new = Book.new
   end
 
   def show
     @user = User.find(params[:id])
-    @book = Book.new
+    @books = @user.books
+    @book_new = Book.new
   end
 
   def edit
